5afcae6c04404232863d32b34ddba4aa2df54b44
harika2907 <harikamandadi6@gmail.com>
2025-01-23 11:53:26 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,9 @@
+integer_var = 42
+float_var = 3.14
+string_var = "Harika Mandadi"
+boolean_var = True
+
+print("Integer Variable:", integer_var, "| Type:", type(integer_var))
+print("Float Variable:", float_var, "| Type:", type(float_var))
+print("String Variable:", string_var, "| Type:", type(string_var))
+print("Boolean Variable:", boolean_var, "| Type:", type(boolean_var))
